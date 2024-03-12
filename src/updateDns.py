19fd6b3b448fb8a8b7b7476c294ff39813bcca84
Jonah.Wille <jonah.wille@gea.com>
2024-03-12 16:57:47 +0100

    added Test part

--- src/updateDns.py
+++ src/updateDns.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv
 IPFILE = os.path.join(os.getcwd(), 'currentIP.txt')
 load_dotenv()
 
+
 def getLoginCookies():
     payload = {'FORM_SUBMIT': 'tl_login',
                'username': os.getenv('username'), 'password': os.getenv('password')}
@@ -44,28 +45,33 @@ def updateSubdomain(auth, ipv4='', ipv6=''):
 
     return r
 
+
 def updateIPs(ip):
-        auth = getLoginCookies()
-        requestsresults = [updateDomain(auth=auth, ipv4=ip),
-            updateSubdomain(auth=auth, ipv4=ip)]
-        for r in requestsresults:
-                if not 'erfolgreich' in str(r.content):
-                        print("error")
+    auth = getLoginCookies()
+    requestsresults = [updateDomain(auth=auth, ipv4=ip),
+                       updateSubdomain(auth=auth, ipv4=ip)]
+    for r in requestsresults:
+        if not 'erfolgreich' in str(r.content):
+            print("error")
+
 
 if __name__ == "__main__":
-        curIP = requests.get('https://api.ipify.org').content.decode('utf8')
-        print(f"running DNS update {os.getcwd()} {curIP}")
+    curIP = requests.get('https://api.ipify.org').content.decode('utf8')
+    if "TEST" in os.environ:
+        print(f"TEST IP: {curIP}")
+        exit()
+    print(f"running DNS update {os.getcwd()} {curIP}")
 
-        if os.path.isfile(IPFILE):
-                with open(IPFILE, "r") as f:
-                        lastIP = f.read()
-        else:
-                lastIP = None
-                print(os.path.abspath(__file__))
-                print(f"IP-file {IPFILE}")
-                print("Keine letzte IP gefunfen!")
+    if os.path.isfile(IPFILE):
+        with open(IPFILE, "r") as f:
+            lastIP = f.read()
+    else:
+        lastIP = None
+        print(os.path.abspath(__file__))
+        print(f"IP-file {IPFILE}")
+        print("Keine letzte IP gefunfen!")
 
-        if not curIP == lastIP:
-                updateIPs(curIP)
-                with open(IPFILE, "w") as f:
-                        f.write(curIP)
+    if not curIP == lastIP:
+        updateIPs(curIP)
+        with open(IPFILE, "w") as f:
+            f.write(curIP)
