849fe6204a23e2aeaf1198bb4d0ab7d3ca8d23e4
Jonah Wille <jonah.wille10@gmail.com>
2024-03-12 10:48:10 +0100

    Update updateDns.py

--- updateDns.py
+++ updateDns.py
@@ -2,8 +2,7 @@ import requests
 import os
 from dotenv import load_dotenv
 
-DIR = "/root/dyndns/"
-
+IPFILE = './currentIP.txt'
 load_dotenv()
 
 def getLoginCookies():
@@ -50,21 +49,21 @@ def updateIPs(ip):
         requestsresults = [updateDomain(auth=auth, ipv4=ip),
             updateSubdomain(auth=auth, ipv4=ip)]
         for r in requestsresults:
-                if 'erfolgreich' in str(r.content):
-                        print("Sucessfull")
-                else:
+                if not 'erfolgreich' in str(r.content):
                         print("error")
 
 if __name__ == "__main__":
         curIP = requests.get('https://api.ipify.org').content.decode('utf8')
         print(f"running DNS update {os.getcwd()} {curIP}")
 
-        with open(DIR + "currentIP.txt", "r") as f:
-                lastIP = f.read()
+        if os.path.isfile(IPFILE):
+                with open(IPFILE, "r") as f:
+                        lastIP = f.read()
+        else:
+                lastIP = None
+                print("Keine letzte IP gefunfen!")
 
         if not curIP == lastIP:
                 updateIPs(curIP)
-                with open(DIR + "currentIP.txt", "w") as f:
+                with open("./currentIP.txt", "w") as f:
                         f.write(curIP)
-        else:
-                print("same IP")
